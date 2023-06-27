dca0f3f6d038365cd1de226d0d794fa23d664777
Vijay Srinivas <vijayraghav22@gmail.com>
2023-06-27 17:59:34 +0530

    add version sort logic

--- ui/packages/consul-ui/app/sort/comparators/node.js
+++ ui/packages/consul-ui/app/sort/comparators/node.js
@@ -38,6 +38,44 @@ export default ({ properties }) =>
             return 0;
         }
       };
+    } else if (key.startsWith('Version:')) {
+      return function (itemA, itemB) {
+        const [, dir] = key.split(':');
+        let a, b;
+        if (dir === 'asc') {
+          a = itemA;
+          b = itemB;
+        } else {
+          b = itemA;
+          a = itemB;
+        }
+
+        // Split the versions into arrays of numbers
+        const versionA = a.Version.split('.').map((part) => {
+          const number = Number(part);
+          return isNaN(number) ? 0 : number;
+        });
+        const versionB = b.Version.split('.').map((part) => {
+          const number = Number(part);
+          return isNaN(number) ? 0 : number;
+        });
+
+        const minLength = Math.min(versionA.length, versionB.length);
+
+        for (let i = 0; i < minLength; i++) {
+          const diff = versionA[i] - versionB[i];
+          switch (diff) {
+            case diff > 0:
+              return 1;
+            case diff < 0:
+              return -1;
+            default:
+              break;
+          }
+        }
+
+        return versionA.length - versionB.length;
+      };
     }
     return properties(['Node'])(key);
   };
